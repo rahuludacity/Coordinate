ec10ac1b399d7245f40c36d720a5b3a828cffe07
rahuludacity <dandamudi.rahul@yahoo.com>
2016-05-25 01:45:54 -0400

    Adding Framework package

---
+++ app/src/main/java/com/coordinate/core/framework/BaseCoordinateActivity.java
@@ -0,0 +1,9 @@
+package com.coordinate.core.framework;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rahul Dandamudi
+ */
+public abstract class BaseCoordinateActivity extends AppCompatActivity {
+}
